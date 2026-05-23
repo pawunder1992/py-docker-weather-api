b5c6d40e1c36e97996f46ed9af445ff8334985e9
pawunder1992 <pavelkryvutsa@gmail.com>
2026-05-23 13:32:05 +0300

    fixed pep8

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ load_dotenv()
 
 
 def get_weather() -> None:
-    URL = "http://api.weatherapi.com/v1/current.json"
+    url = "http://api.weatherapi.com/v1/current.json"
     payload_params = {
         "q": "Paris",
         "key": os.getenv("WEATHER_API_KEY"),
     }
-    result = requests.get(URL, params=payload_params)
+    result = requests.get(url, params=payload_params)
     data = result.json()
     print(
         f"{data['location']['name']}/{data['location']['country']} "
